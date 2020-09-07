21c950d2a81c1447db0f0be0931f17819d3ecce5
Aglicheev Mikhail <toba.ru@mail.ru>
2020-09-07 18:14:39 +0300

    Fixed some problems, added report.

--- ПНАОКИШ/Practice/Practice/RowOperations.cpp
+++ ПНАОКИШ/Practice/Practice/RowOperations.cpp
@@ -10,6 +10,6 @@ void DBMSFuncs::DBTableTxt::RemoveRow(int rowIndex) {
 void DBMSFuncs::DBTableTxt::EditRow(int rowIndex, const string& columnName, string& value) {
 	if (rowIndex < GetSize()) {  //  Just in case.
 		delete (*this)[rowIndex][columnName];
-		(*this)[rowIndex][columnName] = GetValue(value, columnName, GetHeader());  //  FIX
+		(*this)[rowIndex][columnName] = GetValue(value, columnName, GetHeader());
 	}
 }
