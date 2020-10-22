ed1724ecd51feb8e675b9b8aef36d477b65101dd
Aglicheev Mikhail <toba.ru@mail.ru>
2020-10-22 22:53:20 +0300

    BCIT Lab3 is done

---
+++ БКИТ/Лабораторная № 3/SimpleList/SimpleStack.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleListProject
+{
+    public class SimpleListItem<T>
+    {
+        /// <summary>
+        /// Данные
+        /// </summary>
+        public T data { get; set; }
+        /// <summary>
+        /// Следующий элемент
+        /// </summary>
+        public SimpleListItem<T> next { get; set; }
+        ///конструктор
+        public SimpleListItem(T param)
+        {
+            this.data = param;
+        }
+    }
+
+    /// <summary>
+    /// Список
+    /// </summary>
+    public class SimpleList<T> : IEnumerable<T>
+    where T : IComparable
+    {
+        /// <summary>
+        /// Первый элемент списка
+        /// </summary>
+        protected SimpleListItem<T> first = null;
+        /// <summary>
+        /// Последний элемент списка
+        /// </summary>
+        protected SimpleListItem<T> last = null;
+        /// <summary>
+        /// Количество элементов
+        /// </summary>
+        public int Count
+        {
+            get { return _count; }
+            protected set { _count = value; }
+        }
+        int _count;
+
+        /// <summary>
+        /// Добавление элемента
+        /// </summary>
+        public void Add(T element)
+        {
+            SimpleListItem<T> newItem =
+            new SimpleListItem<T>(element);
+            this.Count++;
+
+            //Добавление первого элемента
+            if (last == null)
+            {
+                this.first = newItem;
+                this.last = newItem;
+            }
+            //Добавление следующих элементов
+            else
+            {
+                //Присоединение элемента к цепочке
+                this.last.next = newItem;
+                //Присоединенный элемент считается последним
+                this.last = newItem;
+            }
+        }
+
+        /// <summary>
+        /// Удаление элемента
+        /// </summary>
+        public void Remove(int index)
+        {
+            if ((index < 0) || (index >= this.Count))
+            {
+                //Можно создать собственный класс исключения
+                throw new Exception("Выход за границу индекса");
+            }
+
+            //Удаление элемента из начала списка
+            if (index == 0)
+            {
+                this.first = this.first.next;
+            }
+            else
+            {
+                SimpleListItem<T> current = this.first;
+                for (int i = 0; i < index - 1; ++i)
+                {
+                    current = current.next;
+                }
+                current.next = current.next.next;
+
+                //Меняем last если удаляемый элемент был последним
+                if (index == this.Count - 1)
+                {
+                    last = current;
+                }
+            }
+
+            --this.Count;
+        }
+
+        /// <summary>
+        /// Чтение контейнера с заданным номером
+        /// </summary>
+        public SimpleListItem<T> GetItem(int index)
+        {
+            if ((index < 0) || (index >= this.Count))
+            {
+                //Можно создать собственный класс исключения
+                throw new Exception("Выход за границу индекса");
+            }
+            SimpleListItem<T> current = this.first;
+            
+            for (int i = 0; i < index; ++i)
+            {
+                current = current.next;
+            }
+            return current;
+        }
+
+        /// <summary>
+        /// Чтение элемента с заданным номером
+        /// </summary>
+        public T Get(int number)
+        {
+            return GetItem(number).data;
+        }
+
+        /// <summary>
+        /// Для перебора коллекции
+        /// </summary>
+        public IEnumerator<T> GetEnumerator()
+        {
+            SimpleListItem<T> current = this.first;
+            //Перебор элементов
+            while (current != null)
+            {
+                //Возврат текущего значения
+                yield return current.data;
+                //Переход к следующему элементу
+                current = current.next;
+            }
+        }
+
+        //Реализация обобщенного IEnumerator<T> требует реализации необобщенного интерфейса
+        //Данный метод добавляется автоматически при реализации интерфейса
+        System.Collections.IEnumerator
+        System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Cортировка
+        /// </summary>
+        public void Sort()
+        {
+            Sort(0, this.Count - 1);
+        }
+        /// <summary>
+        /// Алгоритм быстрой сортировки
+        /// </summary>
+        private void Sort(int low, int high)
+        {
+            int i = low;
+            int j = high;
+            T x = Get((low + high) / 2);
+            do
+            {
+                while (Get(i).CompareTo(x) < 0) ++i;
+                while (Get(j).CompareTo(x) > 0) --j;
+                if (i <= j)
+                {
+                    Swap(i, j);
+                    i++; j--;
+                }
+            } while (i <= j);
+        if (low < j) Sort(low, j);
+            if (i < high) Sort(i, high);
+        }
+        /// <summary>
+        /// Вспомогательный метод для обмена элементов при сортировке
+        /// </summary>
+        private void Swap(int i, int j)
+        {
+            SimpleListItem<T> ci = GetItem(i);
+            SimpleListItem<T> cj = GetItem(j);
+            T temp = ci.data;
+            ci.data = cj.data;
+            cj.data = temp;
+        }
+    }
+    /// <summary>
+    /// Стэк
+    /// </summary>
+    public class SimpleStack<T>: SimpleList<T>
+    where T:IComparable
+    {
+        public void Push (T element)
+        {
+            base.Add(element);
+        }
+
+        public T Pop()
+        {
+            T result = base.GetItem(Count - 1).data;
+            base.Remove(Count - 1);
+            return result;
+        }
+    }
+}
--- БКИТ/Лабораторная № 3/Лабораторная № 3/Program.cs
+++ БКИТ/Лабораторная № 3/Лабораторная № 3/Program.cs
@@ -1,10 +1,176 @@
 ﻿using System;
+using System.Text;
 using static System.Math;
 using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using SimpleListProject;
 
 namespace Лабораторная___3
 {
-    abstract class Figure : IPrint, IComparable<object>
+    public class Matrix<T>
+    {
+        /// <summary>
+        /// Словарь для хранения значений
+        /// </summary>
+        Dictionary<string, T> _matrix = new Dictionary<string, T>();
+        /// <summary>
+        /// Количество элементов по горизонтали (максимальное количество столбцов)
+        /// </summary>
+        int maxX;
+        /// <summary>
+        /// Количество элементов по вертикали (максимальное количество строк)
+        /// </summary>
+        int maxY;
+        /// <summary>
+        /// Количество элементов по оси Z
+        /// </summary>
+        int maxZ;
+        /// <summary>
+        /// Реализация интерфейса для проверки пустого элемента
+        /// </summary>
+        IMatrixCheckEmpty<T> сheckEmpty;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public Matrix(int px, int py, int pz,
+        IMatrixCheckEmpty<T> сheckEmptyParam)
+        {
+            this.maxX = px;
+            this.maxY = py;
+            this.maxZ = pz;
+            this.сheckEmpty = сheckEmptyParam;
+        }
+        /// <summary>
+        /// Индексатор для доступа к данных
+        /// </summary>
+        public T this[int x, int y, int z]
+        {
+            set
+            {
+                CheckBounds(x, y, z);
+                string key = DictKey(x, y, z);
+                this._matrix.Add(key, value);
+            }
+            get
+            {
+                CheckBounds(x, y, z);
+                string key = DictKey(x, y, z);
+                if (this._matrix.ContainsKey(key))
+                {
+                    return this._matrix[key];
+                }
+                else
+                {
+                    return this.сheckEmpty.getEmptyElement();
+                }
+            }
+        }
+        /// <summary>
+        /// Проверка границ
+        /// </summary>
+        void CheckBounds(int x, int y, int z)
+        {
+            if (x < 0 || x >= this.maxX)
+            {
+                throw new ArgumentOutOfRangeException("x",
+                "x=" + x + " выходит за границы");
+            }
+            if (y < 0 || y >= this.maxY)
+            {
+                throw new ArgumentOutOfRangeException("y",
+                "y=" + y + " выходит за границы");
+            }
+            if (z < 0 || z >= this.maxZ)
+            {
+                throw new ArgumentOutOfRangeException("z",
+                "z=" + z + " выходит за границы");
+            }
+        }
+        /// <summary>
+        /// Формирование ключа
+        /// </summary>
+        string DictKey(int x, int y, int z)
+        {
+            return x.ToString() + "_" + y.ToString() + "_" + z.ToString();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            for (int k = 0; k < maxZ; ++k)
+            {
+                result.Append(k);
+                result.Append("-й слой матрицы:\n");
+                for (int i = 0; i < maxX; ++i)
+                {
+                    result.Append('[');
+                    for (int j = 0; j < maxY; ++j)
+                    {
+                        if (!сheckEmpty.checkEmptyElement(this[i, j, k]))
+                        {
+                            result.Append(this[i, j, k].ToString());
+                        }
+                        else
+                        {
+                            result.Append("     -     ");
+                        }
+                        if (j < maxY - 1)
+                        {
+                            result.Append("; ");
+                        }
+                    }
+                    result.Append("]\n");
+
+                }
+                result.Append('\n');
+            }
+
+            return result.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Проверка пустого элемента матрицы
+    /// </summary>
+    public interface IMatrixCheckEmpty<T>
+    {
+        /// <summary>
+        /// Возвращает пустой элемент
+        /// </summary>
+        T getEmptyElement();
+        /// <summary>
+        /// Проверка что элемент является пустым
+        /// </summary>
+        bool checkEmptyElement(T element);
+    }
+
+    class FigureMatrixCheckEmpty : IMatrixCheckEmpty<Figure>
+    {
+        /// <summary>
+        /// В качестве пустого элемента возвращается null
+        /// </summary>
+        public Figure getEmptyElement()
+        {
+            return null;
+        }
+        /// <summary>
+        /// Проверка что переданный параметр равен null
+        /// </summary>
+        public bool checkEmptyElement(Figure element)
+        {
+            return element == null;
+            //bool Result = false;
+            //if (element == null)
+            //{
+             //   Result = true;
+            //}
+            //return Result;
+        }
+    }
+
+
+    abstract class Figure : IPrint, IComparable
     {
         protected string Type { get; set; }
 
@@ -83,14 +249,14 @@ namespace Лабораторная___3
 
         public override string ToString()
         {
-            return $"Прямоугольник со сторонами {this.Length} и {this.Width} и площадью {this.Area()}";
+            return $"Прямоугольник со сторонами {this.Length} и {this.Width} и площадью {this.Area():G3}";
         }
     }
 
     class Square : Rectangle
     {
         public Square(double side = 0) : base(side, side) {; }
-        //Ask how to ban changing length.
+
         public override double Length
         {
             get { return _Length; }
@@ -127,7 +293,7 @@ namespace Лабораторная___3
 
         public override string ToString()
         {
-            return $"Квадрат со стороной {this.Length} и площадью {this.Area()}";
+            return $"Квадрат со стороной {this.Length} и площадью {this.Area():G3}";
         }
     }
 
@@ -162,34 +328,134 @@ namespace Лабораторная___3
 
         public override string ToString()
         {
-            return $"Круг с радиусом {this.Radius} и площадью {this.Area()}";
+            return $"Круг с радиусом {this.Radius} и площадью {this.Area():G3}";
         }
     }
 
     class Program
     {
+        public enum Options
+        {
+            SortArrayList = 1,
+            SortListFigure = 2,
+            DemSparseMatrix = 3,
+            DemStack = 4,
+            Exit = 5
+        }
+
+        static Options Menu()
+        {
+            Console.WriteLine("Введите номер нужной опции:");
+            Console.WriteLine("1: Сортировка на основе ArrayList");
+            Console.WriteLine("2: Сортировка на основе List<Figure>");
+            Console.WriteLine("3: Демонстрация разряженной матрицы");
+            Console.WriteLine("4: Демонстрация стека на основе односвязного списка");
+            Console.WriteLine("5: Выход");
+
+            int option = 0;
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("Неправильный ввод. Повторите ввод");
+                }
+                else if (!Enum.IsDefined(typeof(Options), option))
+                {
+                    Console.WriteLine("Неизвестная опция. Повторите ввод");
+                }
+                else
+                {
+                    return (Options)option;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             Rectangle rectangle = new Rectangle(2.4, 67);
             Square square = new Square(1);
             Circle circle = new Circle(1);
-            ArrayList list = new ArrayList {
-                square,
-                rectangle,
-                circle
-            };
 
-            Console.WriteLine("До сортировки:");
-            foreach (Figure figure in list)
-            {
-                figure.Print();
-            }
+            Options option = Menu();
 
-            list.Sort();
-            Console.WriteLine("После сортировки:");
-            foreach (Figure figure in list)
+            while (option != Options.Exit)
             {
-                figure.Print();
+                switch (option)
+                {
+                    case Options.SortArrayList:
+                        {
+                            ArrayList nonGenericList = new ArrayList {
+                            square,
+                            rectangle,
+                            circle
+                        };
+
+                            Console.WriteLine("До сортировки:");
+                            foreach (Figure figure in nonGenericList)
+                            {
+                                figure.Print();
+                            }
+                            Console.Write("\n");
+
+                            nonGenericList.Sort();
+                            Console.WriteLine("После сортировки (ArrayList):");
+                            foreach (Figure figure in nonGenericList)
+                            {
+                                figure.Print();
+                            }
+                            Console.Write("\n");
+                            break;
+                        }
+
+                    case Options.SortListFigure:
+                        List<Figure> GenericList = new List<Figure> {
+                            square,
+                            rectangle,
+                            circle
+                        };
+
+                        Console.WriteLine("До сортировки:");
+                        foreach (Figure figure in GenericList)
+                        {
+                            figure.Print();
+                        }
+                        Console.Write("\n");
+
+                        GenericList.Sort();
+                        Console.WriteLine("После сортировки (List<Figure>):");
+                        foreach (Figure figure in GenericList)
+                        {
+                            figure.Print();
+                        }
+                        Console.Write("\n");
+                        break;
+
+                    case Options.DemSparseMatrix:
+                        Matrix<Figure> matrix = new Matrix<Figure>(2, 2, 2, new FigureMatrixCheckEmpty());
+                        matrix[0, 1, 1] = square;
+                        matrix[1, 0, 1] = rectangle;
+                        matrix[1, 1, 0] = circle;
+                        Console.WriteLine(matrix.ToString());
+                        break;
+
+                    case Options.DemStack:
+                        SimpleStack<Figure> stack = new SimpleStack<Figure>();
+                        Console.WriteLine($"Добавляем {square.ToString()} в стэк");
+                        stack.Push(square);
+                        Console.WriteLine($"Добавляем {rectangle.ToString()} в стэк");
+                        stack.Push(rectangle);
+                        Console.WriteLine($"Добавляем {circle.ToString()} в стэк");
+                        stack.Push(circle);
+
+                        for (int i = 0; i < 3; ++i)
+                        {
+                            Console.WriteLine($"Производим операцию Pop() и удаляем {stack.Pop().ToString()} из стэка");
+                        }
+                        break;
+                }
+
+                Console.Write('\n');
+                option = Menu();
             }
         }
     }
